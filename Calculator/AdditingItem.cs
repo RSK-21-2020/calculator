2ef8740e9817ede43fe89ecd912706738ac18b70
zxerrinor <zxerrinor@gmail.com>
2021-02-11 16:05:58 +0300

    Класс комплексного числа, реализующий 4 базовых операции алгебры.

--- Calculator/AdditingItem.cs
+++
@@ -1,7 +0,0 @@
-namespace Calculator
-{
-    public class AdditingItem
-    {
-        
-    }
-}
---
+++ Calculator/ComplexNumber.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Security.Cryptography;
+
+namespace Calculator
+{
+    public class ComplexNumber
+    {
+        public Double real;
+        public Double imagine;
+        
+        public ComplexNumber(Double re, Double im)
+        {
+            this.real = re;
+            this.imagine = im;
+        }
+
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.real + b.real, a.imagine + b.imagine);
+        }
+        
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.real - b.real, a.imagine - b.imagine);
+        }
+        
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.real * b.real - a.imagine * b.imagine, a.real * b.imagine + a.imagine * b.real);
+        }
+        
+        public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+        {
+            var a1 = a.real;
+            var b1 = a.imagine;
+            var a2 = b.real;
+            var b2 = b.imagine;
+            return new ComplexNumber((a1 * a2 + b1 * b2)/(a2 * a2 + b2 * b2), (a2 * b1 - a1 * b2)/(a2 * a2 + b2 * b2));
+        }
+
+        // public static int power(int x, int y)
+        // {
+        //     if (y == 1)
+        //     {
+        //         return x;
+        //     }
+        //     else
+        //     {
+        //         return x * power(x, y - 1);
+        //     }
+        // }
+
+    }
+}
