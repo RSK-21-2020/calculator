56fd4c7d7b7e66567dbe741fb4fcedec37ec81c4
zxerrinor <zxerrinor@gmail.com>
2021-02-12 09:51:22 +0300

    Refactor

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,45 +4,37 @@ using System.Collections.Generic;
 namespace Calculator {
     internal class Program {
         private static void Main(string[] args) {
-            // var c1 = new ComplexNumber(10, 5);
-            // var c2 = new ComplexNumber(2, -4);
-            // var c3 = c1 / c2;
+            // string currentRawPart = "";
+            println("Введите первое число:");
+            var c1 = new ExpressionPart(Console.ReadLine().Replace(" ", "")).toComplexNumber();
+            println("Введите второе число:");
+            var c2 = new ExpressionPart(Console.ReadLine().Replace(" ", "")).toComplexNumber();
+            println("Введите действие:");
+            switch (Console.ReadLine()) {
+                case "+": println("Результат: " + (c1 + c2)); break;
+                case "-": println("Результат: " + (c1 - c2)); break;
+                case "*": println("Результат: " + c1 * c2); break;
+                case "/": println("Результат: " + c1 / c2); break;
+            }
+
+            // List<ExpressionPart> parts = new List<ExpressionPart>();
+            // foreach (var i in rawInput1) {
+            //     if (i == '(' || i == ')') {
+            //         parts.Add(new ExpressionPart(currentRawPart));
+            //         currentRawPart = "";
+            //     } else {
+            //         currentRawPart += i;
+            //     }
+            // }
             //
-            // Console.WriteLine(c3.real + " " + c3.imagine + "i");
-            var rawInput1 = Console.ReadLine().Replace(" ", "").Split("+");
-            var rawElemList = new List<AddableItem>();
-            foreach (var elem in rawInput1) {
-                if (elem.Contains("-")) {
-                    var firstSecondaryElem = true;
-                    foreach (var elem2 in elem.Split("-")) {
-                        if (firstSecondaryElem) {
-                            addToList(rawElemList, elem2, true);
-                            firstSecondaryElem = false;
-                        } else {
-                            addToList(rawElemList, elem2, false);
-                        }
-                    }
-                } else {
-                    addToList(rawElemList, elem, true);
-                }
-            }
-
-            foreach (var elem in rawElemList)
-                println(elem.isNotNegative
-                    ? elem.isImagine ? elem.value + "i" : elem.value
-                    : "-" + (elem.isImagine ? elem.value + "i" : elem.value));
+            //
+            // foreach (var j in part.expression)
+            //     println(j.isNotNegative
+            //         ? j.isImagine ? j.value + "i" : j.value
+            //         : "-" + (j.isImagine ? j.value + "i" : j.value));
         }
 
-        private static void addToList(List<AddableItem> list, string elem, bool isNotNegative) {
-            if(elem == "") return;
-            if (elem.Contains("i") || elem.Contains("I")) {
-                var i = elem.Replace("i", "").Replace("I", "");
-                list.Add(new AddableItem(double.Parse(i), true, isNotNegative));
-            } else {
-                list.Add(new AddableItem(double.Parse(elem), false, isNotNegative));
-            }
-        }
-
-        private static void println(Object obj) => Console.WriteLine(obj.ToString());
+        public static void println(Object obj) => Console.WriteLine(obj.ToString());
+        public static void print(Object obj) => Console.Write(obj.ToString());
     }
 }
