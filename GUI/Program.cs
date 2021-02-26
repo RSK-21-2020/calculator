70f7646187f97368d5db3e9783ca91683ad1ec19
zxerrinor <zxerrinor@gmail.com>
2021-02-26 10:08:02 +0300

    Добавлен проект графического интерфейса на AvaloniaUI

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-using Calculator;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 
 namespace GUI {
     class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            var num1 = new ComplexNumber(1, 1);
-        }
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
     }
 }
